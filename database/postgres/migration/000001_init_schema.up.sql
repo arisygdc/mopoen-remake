15204e5a5f6ece57a7b9748a28e9d5cab5b33780
arisygdc <arisycso@gmail.com>
2022-04-16 22:11:17 +0700

    relation

--- database/postgres/migration/000001_init_schema.up.sql
+++ database/postgres/migration/000001_init_schema.up.sql
@@ -40,3 +40,23 @@ CREATE TABLE monitoring_data (
     monitoring_terdaftar uuid NOT NULL,
     value FLOAT NOT NULL
 );
+
+-- Relation table
+ALTER TABLE kabupaten ADD FOREIGN KEY (provinsi_id) REFERENCES provinsi(id);
+ALTER TABLE kecamatan ADD FOREIGN KEY (kabupaten_id) REFERENCES kabupaten(id);
+ALTER TABLE desa ADD FOREIGN KEY (kecamatan_id) REFERENCES kecamatan(id);
+
+ALTER TABLE monitoring_terdaftar ADD FOREIGN KEY (tipe_sensor_id) REFERENCES tipe_sensor(id);
+ALTER TABLE monitoring_terdaftar ADD FOREIGN KEY (lokasi_id) REFERENCES desa(id);
+
+ALTER TABLE monitoring_data ADD FOREIGN KEY (monitoring_terdaftar) REFERENCES monitoring_terdaftar(id);
+
+-- Index reference
+CREATE INDEX ON kabupaten (provinsi_id);
+CREATE INDEX ON kecamatan (kabupaten_id);
+CREATE INDEX ON desa (kecamatan_id);
+
+CREATE INDEX ON monitoring_terdaftar (tipe_sensor_id);
+CREATE INDEX ON monitoring_terdaftar (lokasi_id);
+
+CREATE INDEX ON monitoring_data (monitoring_terdaftar);
