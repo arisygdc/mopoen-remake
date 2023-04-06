2243040d1ae912aebd5e5d80050f36203872d468
arisygdc <arisycso@gmail.com>
2023-04-06 13:49:51 +0700

    route now build on server

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"log"
 	"mopoen-remake/config"
-	"mopoen-remake/controller"
-	"mopoen-remake/router"
 	"mopoen-remake/server"
-	svc "mopoen-remake/service"
 )
 
 func main() {
@@ -15,15 +12,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	service, err := svc.New(env)
-	if err != nil {
-		log.Fatal()
-	}
-
 	s := server.New(env)
-	router.New(
-		s.Engine,
-		controller.New(service),
-	)
+	s.ExposeRoute()
 	s.Run()
 }
--- server/server.go
+++ server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"mopoen-remake/config"
+	"mopoen-remake/controller/frontend"
+	"mopoen-remake/controller/sensorgateway"
 	"mopoen-remake/server/middleware"
+	svc "mopoen-remake/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,6 +26,38 @@ func New(env config.Environment) (server Server) {
 	}
 
 	return
+}
+
+func (svr Server) ExposeRoute() error {
+	router := svr.Engine
+	service, err := svc.New(svr.env)
+	if err != nil {
+		return err
+	}
+
+	lokasiController := frontend.NewLokasiController(service)
+	sensorController := frontend.NewSensorController(service)
+	monitoringController := frontend.NewMonitoringController(service)
+
+	ruoterApiV1 := router.Group("/api/v1").Use(middleware.Bearear())
+	ruoterApiV1.POST("/sensor", sensorController.CreateNewTipeSensor)
+	ruoterApiV1.DELETE("/sensor", sensorController.DeleteTipeSensor)
+	ruoterApiV1.GET("/sensor/:id", sensorController.GetTipeSensorByID)
+	ruoterApiV1.GET("/sensors", sensorController.GetAllTipeSensor)
+	ruoterApiV1.POST("/lokasi/:tipe", lokasiController.CreateLokasi)
+	ruoterApiV1.DELETE("/lokasi/:tipe", lokasiController.DeleteLokasi)
+	ruoterApiV1.GET("/lokasi/:tipe", lokasiController.GetAllLokasiWithType)
+	ruoterApiV1.POST("/monitoring/daftar", monitoringController.DaftarMonitoring)
+	ruoterApiV1.GET("/monitoring/terdaftar", monitoringController.GetTerdaftar)
+	ruoterApiV1.GET("/monitoring/terdaftar/:uuid", monitoringController.GetTerdaftarByUUID)
+	ruoterApiV1.GET("/monitoring/value/:uuid", monitoringController.GetData)
+	ruoterApiV1.GET("/monitoring/analisa/:uuid", monitoringController.GetAnalisa)
+	ruoterApiV1.GET("/monitoring/csv/:uuid", monitoringController.ExportAndDownload)
+
+	sensorG := sensorgateway.New(service)
+	sensorGRoute := router.Group("/api/sensor")
+	sensorGRoute.POST("/value", sensorG.SaveDataFromSensor)
+	return nil
 }
 
 func (svr Server) Run() {
